3c1f321d0c0e72da163254542fdc2bb3e999f0ae
prachiV84 <prachimagdum84@gmail.com>
2024-05-22 12:00:19 +0530

    ''created appbar"

---
+++ lib/presentation/screens/main_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'WhatsApp',
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.camera, color: Colors.white),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: const Icon(Icons.more_vert, color: Colors.white),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        body:const Text("mobile screen body..."),
+      ),
+    );
+  }
+}
